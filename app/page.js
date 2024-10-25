c043937cabd9b72c601f9609d025152dce551077
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-25 20:03:13 +0300

    feat: update page structure for main layout and authentication

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
-
+import Main from "../Components/Main";
+import Hero from "../Components/Home/Hero";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -22,7 +23,7 @@ export default function Home() {
           </li>
           <li>Track your mood and see your emotional trends instantly.</li>
         </ol>
-
+        <Hero />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -79,6 +80,6 @@ export default function Home() {
           Contact Us →
         </a>
       </footer>
-    </div>
+    </Main>
   );
 }
