753c4c6597b15e95729f75af7def20674945ae5e
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-25 20:04:33 +0300

    chore: remove unused HeroSection component

---
+++ Components/Home/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="hero-section">
+      <h1>How are you feeling today ?</h1>
+      <p>Stay updated with the latest news and articles.</p>
+    </div>
+  )
+}
